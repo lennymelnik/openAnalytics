7339f4000fc7dcf120d8ccbdbcfc08e0fe476bd6
Leonard Melnik <leonardmelnik@gmail.com>
2021-07-06 19:21:28 -0400

    update header

--- site/src/Components/Header.js
+++ site/src/Components/Header.js
@@ -4,7 +4,8 @@ const Header = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="/">openAnalytics</a>
+          <a class="navbar-brand" href="/">
+          <i class="fas fa-chart-pie" ></i> openAnalytics</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -17,7 +18,7 @@ const Header = () => {
                 <a class="nav-link" href="/properties">Properties</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link" href="/about">About</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
--- site/src/Containers/Home.js
+++ site/src/Containers/Home.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Home = () => {
     return (
         <div class="px-4 py-5 my-5 text-center">
-    <i class="fas fa-chart-pie" style={{fontSize : 60}}></i>
+    <i class="fas fa-chart-pie" style={{fontSize : 60, }}></i>
     <h1 class="display-5 fw-bold">openAnalytics</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">An open source analytics tool that lets you collect all the information you need. Including events, page views, ip logging, and more.</p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Get Started</button>
+        <a type="button" class="btn btn-outline-secondary btn-lg px-4" href='https://github.com/lennymelnik/openanalytics'>Github</a>
       </div>
     </div>
   </div>
